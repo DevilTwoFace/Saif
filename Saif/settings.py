715ce1b7a5dde8c85198370d5f61c7a0c202cb2e
DevilTwoFace <77245796+DevilTwoFace@users.noreply.github.com>
2021-04-12 18:24:18 +0530

    Update settings.py

--- Saif/settings.py
+++ Saif/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vdopinjo=zq36c_=!j5#pr=iq*mwx$eq_i!aco7xqd!e@+3()q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
